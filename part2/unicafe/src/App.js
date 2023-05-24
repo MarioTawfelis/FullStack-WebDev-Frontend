8c969fc2260550dc72fa7cf65b4e69df491a6f80
Mario <mario.tawfelis@outlook.com>
2023-05-24 16:48:19 +0100

    Added event handlers and feedback counters

--- part2/unicafe/src/App.js
+++ part2/unicafe/src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
 
-function App() {
+const Title = ({ title }) => {
+  console.log(title)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <h1>{title}</h1>
+  )
 }
 
-export default App;
+const Button = (props) => {
+  return (
+    <button onClick={props.handleClick}>{props.text}</button>
+  )
+}
+
+const Statistics = ({ text, count }) => {
+  return (
+    <p>{text} {count}</p>
+  )
+}
+
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+
+  return (
+    <div>
+      <Title title={"Give Feedback"}/>
+
+      <Button handleClick={() => setGood(good + 1)} text={"Good"}/>
+      <Button handleClick={() => setNeutral(neutral + 1)} text={"Neutral"}/>
+      <Button handleClick={() => setBad(bad + 1)} text={"Bad"}/>
+      
+      <Title title={"Statistics"}/>
+
+      <Statistics text={"Good"} count={good}/>
+      <Statistics text={"Neutral"} count={neutral}/>
+      <Statistics text={"Bad"} count={bad}/>
+
+    </div>
+  )
+}
+
+export default App
+
