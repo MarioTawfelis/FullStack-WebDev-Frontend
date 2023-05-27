2b905a7b8aa88c92d2fb4559e1cd1f461a5d71fc
Mario <mario.tawfelis@outlook.com>
2023-05-27 15:59:32 +0100

    Declared the Course component as a separate module

--- part3/project-courseinfo/src/App.js
+++ part3/project-courseinfo/src/App.js
@@ -1,48 +1,4 @@
-const Header = (props) => {
-  return (
-    <h2>{props.course.name}</h2>
-  )
-}
-
-const Part = (props) => {
-  return (
-    <p>{props.name} {props.exercises}</p>
-  )
-}
-
-const Content = ({ course }) => {
-  return (
-      <div>
-        {course.parts.map(part =>
-          <Part key={part.id} name={part.name} exercises={part.exercises}/>
-        )}
-      </div>
-  )
-}
-
-const Total = ({ course }) => {
-
-  const total = course.parts.reduce((s, p) => {
-     return s + p.exercises 
-  },0)
-
-
-  return (
-    <b>Number of exercises {total}</b>
-  )
-}
-
-const Course = ({ course }) => {
-  return (
-    <div>
-      <Header course={course}/>
-
-      <Content course={course}/>
-
-      <Total course={course}/>
-    </div>
-  )
-}
+import Course from './components/Course'
 
 const App = () => {
   const courses = [
