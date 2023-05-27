53bf52241f5944472c3d68a9a4a262d6f015fd6c
Mario <mario.tawfelis@outlook.com>
2023-05-27 15:47:35 +0100

    Allows an arbitrary number of courses

--- part3/project-courseinfo/src/App.js
+++ part3/project-courseinfo/src/App.js
@@ -1,6 +1,6 @@
 const Header = (props) => {
   return (
-    <h1>{props.course.name}</h1>
+    <h2>{props.course.name}</h2>
   )
 }
 
@@ -26,7 +26,6 @@ const Total = ({ course }) => {
      return s + p.exercises 
   },0)
 
-  console.log(total)
 
   return (
     <b>Number of exercises {total}</b>
@@ -46,33 +45,58 @@ const Course = ({ course }) => {
 }
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        id: 1,
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        id: 2,
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        id: 3,
-        name: 'State of a component',
-        exercises: 14
-      },
-      {
-        id: 4,
-        name: 'Conditional Rendering',
-        exercises: 5
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
-  return <Course course={course}/>
+  return (
+    <div>
+      <h1>Web development curriculum</h1>
+
+      {courses.map(course => <Course key={course.id} course={course}/>)}
+    </div>
+  )
 }
 
 export default App;
