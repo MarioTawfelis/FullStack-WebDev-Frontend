c5e3a82412e16107aed6cec8c152a4418b22bcbc
Mario <mario.tawfelis@outlook.com>
2023-05-29 12:51:44 +0100

    Added a new input field to provide phone number

--- part3/project-phonebook/src/App.js
+++ part3/project-phonebook/src/App.js
@@ -2,9 +2,12 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas',
+      number: "1234567890"
+    }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -13,17 +16,23 @@ const App = () => {
       alert(`${newName} is already added to phonebook`)
     } else {
       const nameObject = {
-        name: newName
+        name: newName,
+        number: newNumber
       }
   
       setPersons(persons.concat(nameObject))
     }
 
     setNewName('')
+    setNewNumber('')
   }
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
   }
 
   const checkDuplicate = (name) => {
@@ -42,13 +51,16 @@ const App = () => {
           name: <input value={newName} onChange={handleNameChange}/>
         </div>
         <div>
+          number: <input value={newNumber} onChange={handleNumberChange}/>
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       <div>
           {persons.map(person =>
-            <p key={person.name}>{person.name}</p>  
+            <p key={person.name}>{person.name} - {person.number}</p>  
           )}
 
       </div>
